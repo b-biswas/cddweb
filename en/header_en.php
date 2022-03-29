1ca3dc1562c1469ce5b8fb81df835321b844cbf3
Biswajit Biswas <bbiswas.dgp@gmail.com>
2022-03-29 23:48:20 +0200

    remove header

--- en/header_en.php
+++ en/header_en.php
@@ -1,30 +1,3 @@
 <?php
-	if(!empty($_SESSION))
-	{
-	?>
-
-	<div id="loginbox">
-		<button>
-			<a href="index.php?page=profile"> Profile </a>
-		</button>
-		<button>
-			<a href="index.php?page=logout"> Log out </a>
-		</button>
-
-	</div>
-	<?php
-	}
-	else
-	{
-	?>
-	<div id="loginbox">
-		<button>
-			<a href="index.php?page=registration"> Sign up </a>
-		</button>
-		<button>
-			<a href="index.php?page=login"> Sign in </a>
-		</button>
-	</div>
-	<?php
-	}
+	
 ?>
