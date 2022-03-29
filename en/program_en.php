0d9d8a16aa2f5c7285180abbf943adbb987735ba
Biswajit Biswas <bbiswas.dgp@gmail.com>
2022-03-29 23:28:03 +0200

    bug fix program

--- en/program_en.php
+++ en/program_en.php
@@ -1,4 +1,4 @@
 <p>
-    <a href="./Data/CdD2022_program.pdf"  target="_blank">Open the program in a new tab</a>.
+    <a href="./Data/CDD2022_program.pdf"  target="_blank">Open the program in a new tab</a>.
 </p>
-<embed src= "./Data/CdD2022_program.pdf" width= "100%" height= "1000">
+<embed src= "./Data/CDD2022_program.pdf" width= "100%" height= "1000">
