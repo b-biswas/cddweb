a8ace74dd71424a35e4faacf5ab7709ef17db3be
Biswajit Biswas <bbiswas.dgp@gmail.com>
2022-03-29 23:20:49 +0200

    added program

--- en/navigation_en.php
+++ en/navigation_en.php
@@ -32,7 +32,7 @@
 				if(array_key_exists('lang', $_GET))
 				{
 					echo '<li> <a href=./index.php?page=infos&lang=en>Infos</a> </li>';
-					#echo '<li> <a href=./index.php?page=program&lang=en>Program</a> </li>';
+					echo '<li> <a href=./index.php?page=program&lang=en>Program</a> </li>';
 				    #echo '<li> <a href=./index.php?page=jobs&lang=en>Jobs</a> </li>';
 					echo '<li> <a href=./index.php?page=access&lang=en>Access</a> </li>';
 					#echo '<li> <a href=./index.php?page=contact&lang=en>Contact</a> </li>';
@@ -40,7 +40,7 @@
 				else
 				{
 					echo '<li> <a href=./index.php?page=infos>Infos</a> </li>';
-					#echo '<li> <a href=./index.php?page=program>Program</a> </li>';
+					echo '<li> <a href=./index.php?page=program>Program</a> </li>';
 				    #echo '<li> <a href=./index.php?page=jobs>Jobs</a> </li>';
 					echo '<li> <a href=./index.php?page=access>Access</a> </li>';
 					#echo '<li> <a href=./index.php?page=contact>Contact</a> </li>';	
--- en/program_en.php
+++ en/program_en.php
@@ -1,4 +1,4 @@
 <p>
-    <a href="./Data/CdD2020_program.pdf"  target="_blank">Open the program in a new tab</a>.
+    <a href="./Data/CdD2022_program.pdf"  target="_blank">Open the program in a new tab</a>.
 </p>
-<embed src= "./Data/CdD2020_program.pdf" width= "100%" height= "1000">
+<embed src= "./Data/CdD2022_program.pdf" width= "100%" height= "1000">
