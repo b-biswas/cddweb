29d73635e7fc43e1c65d14a17d9f9f54e612ba07
Biswajit Biswas <bbiswas.dgp@gmail.com>
2022-04-01 17:37:07 +0200

    make bold

--- en/outline_en.php
+++ en/outline_en.php
@@ -3,58 +3,58 @@
 Wednesday 13th of April – Micro
 </h2>
 9h30	- Breakfast <br>
-10h 	- François Vannucci (LPNHE), “From the electron to the Higgs boson” – Amphitheatre <br>
+10h 	- <strong>François Vannucci (LPNHE), “From the electron to the Higgs boson” – Amphitheatre <br>
 <br>
 <h3>
 10h30 	- 12h30 Poster session - Hallway
 </h3>
-Gang Lin, “A new pore pressure measurement device for rock in the laboratory” <br>
-Edwan Preau, “Holography and Extreme Phases of Matter” <br>
-Juliette Dupeyron, “Evolution of Fe isotope composition in sedimentary pyrite from Archean to 
+<strong>Gang Lin</strong>, “A new pore pressure measurement device for rock in the laboratory” <br>
+<strong>Edwan Preau</strong>, “Holography and Extreme Phases of Matter” <br>
+<strong>Juliette Dupeyron</strong>, “Evolution of Fe isotope composition in sedimentary pyrite from Archean to 
 Paleoproterozoic: new perspective from microanalyses” <br>
-Etienne Le Glas, “Chlorine isotopes in La Soufrière de Guadeloupe (FWI) fumaroles trace HCl 
+<strong>Etienne Le Glas</strong>, “Chlorine isotopes in La Soufrière de Guadeloupe (FWI) fumaroles trace HCl 
 scrubbing at shallow depth” <br>
-Lucile Mellet, “Hyper-Kamiokande time synchronization system” <br>
-Salomé Pannefieu, “Sodium and magnesium in aluminosilicate melts and glasses: do they get 
+<strong>Lucile Mellet</strong>, “Hyper-Kamiokande time synchronization system” <br>
+<strong>Salomé Pannefieu</strong>, “Sodium and magnesium in aluminosilicate melts and glasses: do they get 
 along?” <br>
-Adrien Donatini, “Fast and Cerium: A study of redox kinetics in aluminosilicate melts” <br>
-Robin Bonnet, “Biochemical Degradation Mechanisms of Cementitious Matrices: Application to 
+<strong>Adrien Donatini</strong>, “Fast and Cerium: A study of redox kinetics in aluminosilicate melts” <br>
+<strong>Robin Bonnet</strong>, “Biochemical Degradation Mechanisms of Cementitious Matrices: Application to 
 Asbestos Cement Wastes” <br>
-Valentina Raskina, “R&D of the High Granularity Timing Detector” <br>
-Martin Colledge, “Influence of Small Sinusoidal Stress Perturbations on Microseismic Triggering at 
+<strong>Valentina Raskina</strong>, “R&D of the High Granularity Timing Detector” <br>
+<strong>Martin Colledge</strong>, “Influence of Small Sinusoidal Stress Perturbations on Microseismic Triggering at 
 the Laboratory Scale” <br>
-Amicie Delahaie, “Rock-Eval®-RMQS: Studying the Characteristics of Soil Organic Matter in 
+<strong>Amicie Delahaie</strong>, “Rock-Eval®-RMQS: Studying the Characteristics of Soil Organic Matter in 
 France with Rock-Eval® 6 Thermal Analysis to Evaluate its Stability” <br>
-Augustin Thomas, “Tidal oscillations in boreholes reveal hydrodynamic as well as elastic properties 
+<strong>Augustin Thomas</strong>, “Tidal oscillations in boreholes reveal hydrodynamic as well as elastic properties 
 of aquifers” <br>
-Haoxuan Sun, “Metal-silicate silicon isotopic fractionation” <br>
+<strong>Haoxuan Sun</strong>, “Metal-silicate silicon isotopic fractionation” <br>
 <br>
 <h3>
 14h – 17h Presentations – Amphitheatre
 </h3>
-Théo Briolet, “Experimental study of microstructural controls on the weathering of carbonate rocks” <br>
-Kun Zheng, “Selective leaching of indium from end-of-life LCD panels by desferrioxamine E”<br>
-Jani Kastikainen, “Investigations of quantum critical systems”<br>
+<strong>Théo Briolet</strong>, “Experimental study of microstructural controls on the weathering of carbonate rocks” <br>
+<strong>Kun Zheng</strong>, “Selective leaching of indium from end-of-life LCD panels by desferrioxamine E”<br>
+<strong>Jani Kastikainen</strong>, “Investigations of quantum critical systems”<br>
 15h15 – Break <br>
-Lukas Maderer, “Measuring the Earth’s density profile with atmospheric neutrinos” <br>
-Xiaoquan Qin, “Geochemistry of trace metals associated with Fe-Mn nodules (FMN) of red soil 
+<strong>Lukas Maderer</strong>, “Measuring the Earth’s density profile with atmospheric neutrinos” <br>
+<strong>Xiaoquan Qin</strong>, “Geochemistry of trace metals associated with Fe-Mn nodules (FMN) of red soil 
 profiles in China” <br>
-Tri Dung Nguyen, “Photometric modeling for the characterization of planetary surfaces”
+<strong>Tri Dung Nguyen</strong>, “Photometric modeling for the characterization of planetary surfaces”
 Runa van Tent, <br>
 <br>
 <h3>
 14h – 17h Presentations – room Outremer
 </h3>
-Emilie Pirot, “Exploring Greenland seismicity with Machine Learning” <br>
-Alice Dupiau, “Modeling spectral and directional soil reflectance in the solar domain (400-2500 nm) 
+<strong>Emilie Pirot</strong>, “Exploring Greenland seismicity with Machine Learning” <br>
+<strong>Alice Dupiau</strong>, “Modeling spectral and directional soil reflectance in the solar domain (400-2500 nm) 
 as a function of moisture content” <br>
-Baptiste Thomas, “Caracterising convective mixing efficiency in a terrestrial magma ocean” <br>
+<strong>Baptiste Thomas</strong>, “Caracterising convective mixing efficiency in a terrestrial magma ocean” <br>
 15h15 – Break <br>
-Samuel Zouari, “Studying the Galactic center and its variability at Very High Energies with HESS” <br>
-Angèle Laurent, “Source Characterisation of the Very Long Period Earthquakes of the Mayotte 
+<strong>Samuel Zouari</strong>, “Studying the Galactic center and its variability at Very High Energies with HESS” <br>
+<strong>Angèle Laurent</strong>, “Source Characterisation of the Very Long Period Earthquakes of the Mayotte 
 ongoing Seismo-Volcanic Crisis” <br>
-Raphaël Bajou, “Assessing rock strength distribution in lava dome with muon tomography” <br>
-Marie Vulliet, “Erosional dynamics of a river driven by groundwater seepage” <br>
+<strong>Raphaël Bajou</strong>, “Assessing rock strength distribution in lava dome with muon tomography” <br>
+<strong>Marie Vulliet</strong>, “Erosional dynamics of a river driven by groundwater seepage” <br>
 <br>
 <h2>
 Thursday 14th of April – Macro
@@ -65,51 +65,54 @@ Thursday 14th of April – Macro
 <h3>
 10h30 – 12h30 Poster session - Hallway <br>
 </h3>
-Anaïs Rusquet, “Volcanism in the Comoros Archipelago : New K-Ar ages and geochemical data” <br>
-Léna Arthur, “Numerical study of accretion flows in a supermassive black hole binary system during 
+<strong>Anaïs Rusquet</strong>, “Volcanism in the Comoros Archipelago : New K-Ar ages and geochemical data” <br>
+<strong>Léna Arthur</strong>, “Numerical study of accretion flows in a supermassive black hole binary system during 
 merger” <br>
-Guy Augarde, “NaCL : Nouvel algorithme pour Courbes de Lumière” <br>
-Léon Vidal, “LISA ON TABLE: Simulating interferometric signals and testing TDI” <br>
-Mohammad Amin Aminian, “Ocean floor seismological and environmental monitoring” <br>
-Raphaël Kou, “Dark matter, galaxies and gas cross-correlations: towards a better understanding of 
+<strong>Guy Augarde</strong>, “NaCL : Nouvel algorithme pour Courbes de Lumière” <br>
+<strong>Léon Vidal</strong>, “LISA ON TABLE: Simulating interferometric signals and testing TDI” <br>
+<strong>Mohammad Amin Aminian</strong>, “Ocean floor seismological and environmental monitoring” <br>
+<strong>Raphaël Kou</strong>, “Dark matter, galaxies and gas cross-correlations: towards a better understanding of 
 our universe large-scale structure” <br>
-Bryan Raimbault, “Coseismic and Early Postseismic deformation of the Mw7.2 Nippes (Haiti) 
+<strong>Bryan Raimbault</strong>, “Coseismic and Early Postseismic deformation of the Mw7.2 Nippes (Haiti) 
 Earthquake” <br>
-Arthur Walbecq, “Characterizing the efficiency of magma oceans degassing” <br>
-Louis-Marie Gauer, “Data-driven gap filling and spatio-temporal filtering of the GRACE-GRACE-
+<strong>Arthur Walbecq</strong>, “Characterizing the efficiency of magma oceans degassing” <br>
+<strong>Louis-Marie Gauer</strong>, “Data-driven gap filling and spatio-temporal filtering of the GRACE-GRACE-
 FO records” <br>
-Laëtitia Pantobe, “Monitoring shallow volcanic seismicity at La Soufrière de Guadeloupe : new 
+<strong>Laëtitia Pantobe</strong>, “Monitoring shallow volcanic seismicity at La Soufrière de Guadeloupe : new 
 constraints from relative location and detection of a cyclic swarm activity” <br>
-Christos Ntinos, “Global infrared mosaics of Enceladus based on new navigation calculations”
-Marouchka Froment, “The seismic source of meteorite impacts” <br>
-Arthur Delorme, “Learning methods for the calculation of deformation maps from optical images” <br>
-Mario Fernandez, “A benchmark study of deep learning methods for seismic interpolation” <br>
-14h – 17h Presentations – Amphitheatre<br>
-Alexandra Perron, “Thermal analyses of biocarbonates as part of the search for traces of life in 
+<strong>Christos Ntinos</strong>, “Global infrared mosaics of Enceladus based on new navigation calculations”
+<strong>Marouchka Froment</strong>, “The seismic source of meteorite impacts” <br>
+<strong>Arthur Delorme</strong>, “Learning methods for the calculation of deformation maps from optical images” <br>
+<strong>Mario Fernandez</strong>, “A benchmark study of deep learning methods for seismic interpolation” <br>
+<br>
+<h3>
+	14h – 17h Presentations – Amphitheatre
+</h3>
+<strong>Alexandra Perron</strong>, “Thermal analyses of biocarbonates as part of the search for traces of life in 
 planetary environments” <br>
-Julien Marchioro, “Searching progenitors of gravitational waves, the evolution of massive binary 
+<strong>Julien Marchioro</strong>, “Searching progenitors of gravitational waves, the evolution of massive binary 
 stars”<br>
-Baptiste Jost, “Novel method for joint systematic correction and foreground cleaning and its 
+<strong>Baptiste Jost</strong>, “Novel method for joint systematic correction and foreground cleaning and its 
 application to the estimation of cosmic birefringence in Simons Observatory.”<br>
 15h15 – Break <br>
-Guest session – PhD candidates from GRNE doctoral school <br>
+<strong>Guest session – PhD candidates from GRNE doctoral school <br>
 <br>
 <h3>
 14h – 17h Presentations – room Outremer
 </h3>
-Boris Maletckii, “The Near Real Time Analysis of Ionospheric Disturbances in a near field by GNSS
+<strong>Boris Maletckii</strong>, “The Near Real Time Analysis of Ionospheric Disturbances in a near field by GNSS
 data” <br>
-Marie Bouih, “Deep mass redistribution prior to the Maule earthquake revealed by GRACE satellite 
+<strong>Marie Bouih</strong>, “Deep mass redistribution prior to the Maule earthquake revealed by GRACE satellite 
 gravity”<br>
-Quang Nam Dam, “Noise propagation in LISA data and model validation with LISANode simulator”
+<strong>Quang Nam Dam</strong>, “Noise propagation in LISA data and model validation with LISANode simulator”
 15h15 – Break <br>
-Nirmit Dhabaria, “An insight into the nature of a 70 Ma old crust and the Moho transition zone 
+<strong>Nirmit Dhabaria</strong>, “An insight into the nature of a 70 Ma old crust and the Moho transition zone 
 across the Saint Paul fracture zone near 18ᵒ W in the Equatorial Atlantic Ocean.” <br>
-Roxane Tissandier, “The Mw 8.3 2015 Illapel afterslip imaged through a time-dependent inversion 
+<strong>Roxane Tissandier</strong>, “The Mw 8.3 2015 Illapel afterslip imaged through a time-dependent inversion 
 of continuous and survey GNSS data” <br>
-Pierre Boymond, “Volcano-tectonic interactions within two recently discovered submarine volcanic 
+<strong>Pierre Boymond</strong>, “Volcano-tectonic interactions within two recently discovered submarine volcanic 
 fields: Implication for geodynamics in the Comoros”<br>
-Soumya Bohidar, “Seismic constraints on hydrothermal circulation and magmato-tectonic 
+<strong>Soumya Bohidar</strong>, “Seismic constraints on hydrothermal circulation and magmato-tectonic 
 interactions beneath Lucky Strike volcano, Mid-Atlantic Ridge (MAR)”<br>
 <br>
 <h2>
@@ -122,54 +125,54 @@ works” – Amphitheatre <br>
 <h3>
 10h30 – 12h30 Poster session - Hallway <br>
 </h3>
-Mathias Regnier, “Bolometric Interferometry and QUBIC: asolution to Galactic foreground 
+<strong>Mathias Regnier</strong>, “Bolometric Interferometry and QUBIC: asolution to Galactic foreground 
 complexity for primordial B-modes search”<br>
-Arianna Rizzieri, “Component separation for the polarized microwave sky in view of next-
+<strong>Arianna Rizzieri</strong>, “Component separation for the polarized microwave sky in view of next-
 generation cosmic microwave background experiments”<br>
-Magdy Morshed, “Partial-sky coverage of the Cosmic Microwave Background polarization in the 
+<strong>Magdy Morshed</strong>, “Partial-sky coverage of the Cosmic Microwave Background polarization in the 
 context of Simons Observatory”<br>
-Alexandre Kompalitch, “Development of biomineralization approaches for the selective metal 
+<strong>Alexandre Kompalitch</strong>, “Development of biomineralization approaches for the selective metal 
 recovery from end-of-life products”<br>
-Chloé Goupy, “Detecting reactor neutrinos with the NUCLEUS Experiment”<br>
-Francesco Biagioli, “Field testing of fiber-optic distributed acoustic sensing (DAS) for volcanic 
+<strong>Chloé Goupy</strong>, “Detecting reactor neutrinos with the NUCLEUS Experiment”<br>
+<strong>Francesco Biagioli</strong>, “Field testing of fiber-optic distributed acoustic sensing (DAS) for volcanic 
 monitoring in Stromboli”<br>
-Antoine Demont, “Rheological influences on the growth of a detachement fault”<br>
-Gabriel Devos, “Cadmium isotope compositions as a geochemical tracer”<br>
-Augustin Maller, “Metal fragmentation by large Earth-Forming impacts”<br>
-Fei Zhou, “Variability of Sea-Surface Magnetic Anomalies at Ultraslow Spreading Centers: 
+<strong>Antoine Demont</strong>, “Rheological influences on the growth of a detachement fault”<br>
+<strong>Gabriel Devos</strong>, “Cadmium isotope compositions as a geochemical tracer”<br>
+<strong>Augustin Maller</strong>, “Metal fragmentation by large Earth-Forming impacts”<br>
+<strong>Fei Zhou</strong>, “Variability of Sea-Surface Magnetic Anomalies at Ultraslow Spreading Centers: 
 Consequence of Detachment Faulting and Contrasted Magmatism?”<br>
-Colin Chanteloube, “Source-to-sink aeolian fluxes from arid landscape dynamics in the Lut Desert”<br>
-Chagnon Glynn, “Brine Vs Marine Water as Sources of Halogen-rich Hydrothermal Fluids at La 
+<strong>Colin Chanteloube</strong>, “Source-to-sink aeolian fluxes from arid landscape dynamics in the Lut Desert”<br>
+<strong>Chagnon Glynn</strong>, “Brine Vs Marine Water as Sources of Halogen-rich Hydrothermal Fluids at La 
 Soufrière de Guadeloupe”<br>
-Maylis de La Serve, “Are there detectable common aperiodic displacements at ITRF co-location 
+<strong>Maylis de La Serve</strong>, “Are there detectable common aperiodic displacements at ITRF co-location 
 sites?”<br>
-Sophie Coural, “Analysis of atmospheric nanoparticles by plane trees”<br>
+<strong>Sophie Coural</strong>, “Analysis of atmospheric nanoparticles by plane trees”<br>
 <br>
 <h3>
 14h – 17h Presentations – Amphitheatre<br>
 </h3>
-Konstantin Leyde, “Current and future constraints on cosmology and modified gravitational wave 
-friction from binary black holes”<br>
-Gaspard Farge, “From the pore scale to the subduction scale -- how transient fluid processes shape 
+<strong>Konstantin Leyde</strong>, “Current and future constraints on cosmology and modified gravitational wave 
+<strong>friction from binary black holes”<br>
+<strong>Gaspard Farge</strong>, “From the pore scale to the subduction scale -- how transient fluid processes shape 
 microseismicity patterns”<br>
-Simon Bufféral, “Investigating the dynamics of the post-alpine dismantling in the Peloponnese 
+<strong>Simon Bufféral</strong>, “Investigating the dynamics of the post-alpine dismantling in the Peloponnese 
 (Greece) - a morphological approach”<br>
 15h15 – Break <br>
-Linru Fang, “Half-life and initial Solar System abundance of 146Sm determined from the oldest 
+<strong>Linru Fang</strong>, “Half-life and initial Solar System abundance of 146Sm determined from the oldest 
 andesitic meteorite”<br>
-Julie Rode, “DarkSide-50: Characterisation of the LAr ionization response in the keV regime”<br>
-Ssu-Ting Lai, “Time-lapse rock deformation monitoring using waveform inversion”<br>
+<strong>Julie Rode</strong>, “DarkSide-50: Characterisation of the LAr ionization response in the keV regime”<br>
+<strong>Ssu-Ting Lai</strong>, “Time-lapse rock deformation monitoring using waveform inversion”<br>
 <br>
 <h3>
 14h – 17h Presentations – room Outremer
 </h3>
-Maël Es-sayeh, “Updated radiative transfer model for titan: validation on vims-cassini observations 
+<strong>Maël Es-sayeh</strong>, “Updated radiative transfer model for titan: validation on vims-cassini observations 
 of the huygens landing site and application to the analysis of the dragonfly landing area”<br>
-Ting Tan, “Detection of Baryon Acoustic Oscillations using Lyman-α Forests in DESI”<br>
-Lea Ostorero, “Correlating diffusive timescales in crystals with monitoring signals leading to 
+<strong>Ting Tan</strong>, “Detection of Baryon Acoustic Oscillations using Lyman-α Forests in DESI”<br>
+<strong>Lea Ostorero</strong>, “Correlating diffusive timescales in crystals with monitoring signals leading to 
 explosive eruptions”<br>
 15h15 – Break <br>
-Sabrina Menina, “Earth-Moon-Mars coda attenuation comparison”<br>
-Souradeep Mahato, “Strain localization in mid-ocean ridge detachment faults: multiscale study of a 
+<strong>Sabrina Menina</strong>, “Earth-Moon-Mars coda attenuation comparison”<br>
+<strong>Souradeep Mahato</strong>, “Strain localization in mid-ocean ridge detachment faults: multiscale study of a 
 nearly amagmatic endmember case (64°40’E Southwest Indian Ridge)”<br>
 </p>
