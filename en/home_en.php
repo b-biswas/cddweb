b9abe6b255b3e700bbe1d85bf5e757e2ee8b986f
Biswajit Biswas <bbiswas.dgp@gmail.com>
2022-04-03 21:38:44 +0200

    update competition data

--- en/home_en.php
+++ en/home_en.php
@@ -6,8 +6,13 @@ The congress is organised by the 1st year PhD students from both
 components of the doctoral school (Environment Sciences and the Physics of the Universe) and will feature vulgarization talks
 from reseachers along with posters and presentations from Ph.D. students of STEP'UP and other invited students.
 The goal of this conference is to establish links between students, researchers and professionals in the geosciences and physics of the universe fields. 
-The <strong>2022 edition will be held at IPGP between 13 and 15 April.</strong></p>
-
+The <strong>2022 edition, 'From Micro to Macro', will be held at IPGP between 13 and 15 April.</strong></p>
+<p>
+For participants, there will also be awards for the best poster!
+A vote for the best poster will be held during the three days of the CDD. 
+The winner will be announced on the last day and will receive a special prize!
+More details on the poster election are provided in the Infos page.
+</p>
 <h3>
 3 days schedule
 </h3>
