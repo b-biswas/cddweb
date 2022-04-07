0ad929d40a40e60ef443d3f5cb00160b04b830fb
Antoine_dmt_dynamics <demontantoine@gmail.com>
2022-04-07 13:21:09 +0200

    Update schedule_en.php

--- en/schedule_en.php
+++ en/schedule_en.php
@@ -40,7 +40,7 @@ of aquifers” <br>
 <strong>Xiaoquan Qin</strong>, “Geochemistry of trace metals associated with Fe-Mn nodules (FMN) of red soil 
 profiles in China” <br>
 <strong>Tri Dung Nguyen</strong>, “Photometric modeling for the characterization of planetary surfaces” <br>
-<strong>Runa van Tent,“Global mantle density tomography using Earth’s free oscillations” <br>
+<strong>Runa van Tent<\strong>,“Global mantle density tomography using Earth’s free oscillations” <br>
 <br>
 <h3>
 14h – 17h Presentations – room Outremer
@@ -180,7 +180,7 @@ of the huygens landing site and application to the analysis of the dragonfly lan
 <strong>Ting Tan</strong>, “Detection of Baryon Acoustic Oscillations using Lyman-α Forests in DESI”<br>
 <strong>Lea Ostorero</strong>, “Correlating diffusive timescales in crystals with monitoring signals leading to 
 explosive eruptions”<br>
-15h15 – Break <br>
+15h05 – Break <br>
 <strong>Sabrina Menina</strong>, “Earth-Moon-Mars coda attenuation comparison”<br>
 <strong>Souradeep Mahato</strong>, “Strain localization in mid-ocean ridge detachment faults: multiscale study of a 
 nearly amagmatic endmember case (64°40’E Southwest Indian Ridge)”<br>
